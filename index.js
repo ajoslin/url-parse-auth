d75e8a0786afbd58e7842b4052a3eaf415cc8801
Andrew Joslin <andrewtjoslin@gmail.com>
2017-06-27 15:58:58 -0600

    Init

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 'use strict'
+
+var url = require('url')
+
+module.exports = function (s) {
+  var result = url.parse(s || '')
+  if (!result.auth) {
+    return {
+      user: undefined,
+      password: undefined
+    }
+  }
+  var split = result.auth.split(':')
+  return {
+    user: split[0],
+    password: split[1] || ''
+  }
+}
--- test.js
+++ test.js
@@ -1,8 +1,24 @@
 'use strict'
 
 var test = require('tape')
-var urlParsePassword = require('./')
+var parse = require('./')
 
 test(function (t) {
-
+  t.deepEqual(parse('https://foo:bar@baz.com'), {
+    user: 'foo',
+    password: 'bar'
+  })
+  t.deepEqual(parse('https://:bar@baz.com'), {
+    user: '',
+    password: 'bar'
+  })
+  t.deepEqual(parse('https://baz.com'), {
+    user: undefined,
+    password: undefined
+  })
+  t.deepEqual(parse(''), {
+    user: undefined,
+    password: undefined
+  })
+  t.end()
 })
